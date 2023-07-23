3b759d7a54e4c7e2a807ff1b25af20713cdc7cb0
Eric Emery <ericemery099@gmail.com>
2023-07-23 02:39:39 +0000

    inital commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { useState } from "react"
+
+
+export default function App() {
+  const [page, setPage] = useState([]);
+
+  if (page === "Home") {
+    return (
+      <>
+      
+      </>
+)
+}
