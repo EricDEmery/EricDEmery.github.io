b76df7423d6918e8626ae5a69f219fddb07df936
Eric Emery <ericemery099@gmail.com>
2023-07-23 06:12:55 +0000

    added about section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { useState } from "react"
 import Navbar from './Navbar';
+import About from './About';
 
 
 export default function App() {
-  const [page, setPage] = useState([]);
+  const [page, setPage] = useState("Home");
 
   if (page === "Home") {
     return (
       <>
         <Navbar setPage={setPage}/>
+        <About />
       </>
     );
   }
