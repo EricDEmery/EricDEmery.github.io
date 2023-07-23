68932a6284232fd0991c8989c4f4fc9657bc8383
Eric Emery <ericemery099@gmail.com>
2023-07-23 20:25:44 +0000

    updated image

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import Eric from './img/Eric.jpg'
+import React from 'react';
+import Eric from './img/Eric.jpg';
 
 export default function About() {
- 
-        return (
-                                                //Base is 540
-          <div className="card mb-3" style={{maxWidth: '1140px'}}>
-            <div className="row g-0">
-              <div className="col-md-4">
-                <img src={Eric} className="img-fluid rounded-start" alt="Me Playing Guitar" />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Eric Emery</h5>
-                  <p className="card-text">Hello! My name is Eric and I'm a 24 year old software developer! I am a happily married father of twin boys named Leo and Jett!</p>
-                  <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
+  return (
+    <div className="card mb-3 d-flex text-center" style={{ maxWidth: '540px', borderRadius: '0.25rem' }}>
+      <div className="row g-0">
+        <div className="col-md-4 d-flex align-items-center">
+          <div
+            style={{
+              backgroundImage: `url(${Eric})`,
+              backgroundSize: 'cover',
+              height: '100%',
+              width: '100%',
+              borderRadius: '0.25rem', // Same as the card's border radius
+            }}
+          ></div>
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">Eric Emery</h5>
+            <p className="card-text">Hello! My name is Eric and I'm a 24-year-old software developer! I am a happily married father of twin boys named Leo and Jett! I specialize in Front End web development, but I'm proficient in Python, SQL, Django, and Django Rest as well.</p>
+            <p className="card-text"><small className="text-body-secondary">All projects included in this portfolio as well as the site itself were developed by yours truly.</small></p>
           </div>
-        );
-      }
+        </div>
+      </div>
+    </div>
+  );
+}
