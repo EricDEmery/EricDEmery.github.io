2cefe34efcf42bc154022393294339d0c89a9ac0
Eric Emery <ericemery099@gmail.com>
2023-09-20 21:01:21 +0000

    button fix

--- my-app/src/Portfolio.js
+++ my-app/src/Portfolio.js
@@ -56,7 +56,7 @@ export default function Project() {
               <div className="card-body">
                 <h5 className="card-title title-font">Django Jams</h5>
                 <p className="card-text text-font">Django Jams is a backend project similar to platforms like Spotify. The program allows you to use CRUD features to update a database with Django REST Framework</p>
-                <a href="https://github.com/EricDEmery/django-jams" className="btn btn-primary mr-2" target="_blank" rel="noopener noreferrer">Source Code</a>
+                <a href="https://github.com/EricDEmery/django-jams" className="btn mr-2" style={sourceCodeButtonStyle} target="_blank" rel="noopener noreferrer">Source Code</a>
               </div>
             </div>
           </div>
@@ -71,8 +71,8 @@ export default function Project() {
               <div className="card-body">
                 <h5 className="card-title title-font">The Rolling Scones</h5>
                 <p className="card-text text-font">The Rolling Scones is a web application designed using React. The site used to pull menu data from a third party API and used a .map array method to place each menu item into a separate bootstrap container. However, it is now connected to the Backend Bistro project you will see below where you can add menu items to a database that will make them appear on the web page.</p>
-                <a href="https://github.com/EricDEmery/React-Random-Restaurant" className="btn btn-primary mr-2" target="_blank" rel="noopener noreferrer">Source Code</a>
-                <a href="https://ericdemery.github.io/React-Random-Restaurant/" className="btn btn-primary mr-2" target="_blank" rel="noopener noreferrer">Github Pages URL</a>
+                <a href="https://github.com/EricDEmery/React-Random-Restaurant" className="btn mr-2" style={sourceCodeButtonStyle} target="_blank" rel="noopener noreferrer">Source Code</a>
+                <a href="https://ericdemery.github.io/React-Random-Restaurant/" className="btn mr-2" style={liveButtonStyle} target="_blank" rel="noopener noreferrer">Github Pages URL</a>
               </div>
             </div>
           </div>
@@ -87,7 +87,7 @@ export default function Project() {
               <div className="card-body">
                 <h5 className="card-title title-font">Backend Bistro</h5>
                 <p className="card-text text-font">Backend Bistro is programmed using Django. It is now the backend for The Rolling Scones.</p>
-                <a href="https://github.com/EricDEmery/Backend-Bistro" className="btn btn-primary pr-2 pb-2" target="_blank" rel="noopener noreferrer">Source Code</a>
+                <a href="https://github.com/EricDEmery/Backend-Bistro" className="btn pr-2 pb-2" style={sourceCodeButtonStyle} target="_blank" rel="noopener noreferrer">Source Code</a>
               </div>
             </div>
           </div>
@@ -102,7 +102,7 @@ export default function Project() {
               <div className="card-body">
                 <h5 className="card-title title-font">SQL Heros</h5>
                 <p className="card-text text-font">SQL Heros is a project based on SQL Queries. The program enables you to Add, Update, Search, and Delete different heroes in the database directly from prompts in the terminal.</p>
-                <a href="https://github.com/EricDEmery/SQL-Heros" className="btn btn-primary mr-2" target="_blank" rel="noopener noreferrer">Source Code</a>
+                <a href="https://github.com/EricDEmery/SQL-Heros" className="btn mr-2" style={sourceCodeButtonStyle} target="_blank" rel="noopener noreferrer">Source Code</a>
               </div>
             </div>
           </div>
